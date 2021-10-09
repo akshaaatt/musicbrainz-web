51314493d61988b7e875a2f81f60e9e2fee980a2
akshaaatt <tiwariakshat03@gmail.com>
2021-10-09 22:11:25 +0530

    Add Picard to downloads

--- src/components/AppDownload/AppDownload.js
+++ src/components/AppDownload/AppDownload.js
@@ -7,7 +7,7 @@ function AppDownload() {
             <div className="container">
                 <div className="row align-items-center">
                     <div className="col-md-6 me-auto text-center text-md-start mb-5 mb-md-0">
-                        <h2>Download and Use our App on Mobile</h2>
+                        <h2>Download and Use our App and Software</h2>
                     </div>
                     <div className="col-md-5 text-center text-md-end">
                         <a href="https://play.google.com/store/apps/details?id=org.metabrainz.android" target="_blank" className="btn d-inline-flex align-items-center">
@@ -15,6 +15,9 @@ function AppDownload() {
                         </a>
                         <a href="https://f-droid.org/en/packages/org.metabrainz.android/" target="_blank" className="btn d-inline-flex align-items-center">
                             <i className="fab fa-android"/><span>F-Droid</span>
+                        </a>
+                        <a href="https://play.google.com/store/apps/details?id=org.metabrainz.android" target="_blank" className="btn d-inline-flex align-items-center">
+                            <i className="fa fa-laptop"/><span>Picard</span>
                         </a>
                     </div>
                 </div>
