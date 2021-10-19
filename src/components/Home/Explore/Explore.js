21ca999b14561f684a2c34ea9316eac8ef97f5ad
akshaaatt <tiwariakshat03@gmail.com>
2021-10-19 05:35:28 +0530

    Final changes

--- src/components/Home/Explore/Explore.js
+++ src/components/Home/Explore/Explore.js
@@ -54,6 +54,19 @@ function Explore(props) {
                             </div>
                         </div>
                     </div>
+
+                    <div className="col-md-6 d-flex" data-aos="fade-left">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title"><a href="https://musicbrainz.org/doc/MusicBrainz_Database" target="_blank">MusicBrainz Database</a></h5>
+                                <p className="card-text">
+                                    The MusicBrainz Database stores all of the various pieces of information we collect about music, from artists and their releases to works and their composers, and much more.
+                                    Most of the data in the MusicBrainz Database is licensed under CC0, which effectively places the data into the Public Domain. The remaining data is released under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 license.
+                                    All our data is available for commercial licensing. If you are interested in licensing this data for commercial use, please contact us.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                     <div className="col-md-6 d-flex" data-aos="fade-left">
                         <div className="card">
                             <div className="card-body">
@@ -64,6 +77,17 @@ function Explore(props) {
                             </div>
                         </div>
                     </div>
+                    <div className="col-md-6 d-flex" data-aos="fade-left">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title"><a href="https://musicbrainz.org/doc/How_Editing_Works" target="_blank">Editing Philosophy</a></h5>
+                                <p className="card-text">
+                                    Maintaining a comprehensive database of all types of music is a large task, and MusicBrainz depends on its users to spot mistakes in the database and then to take the initiative to correct these errors. To help with that task the MusicBrainz editing and voting system was designed, it gives MusicBrainz users the ability to update and maintain the database effectively and easily.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
--- src/components/Home/Supporters/Supporters.js
+++ src/components/Home/Supporters/Supporters.js
@@ -20,49 +20,73 @@ function Supporters(props) {
 
                     <div className="col-lg-3 col-md-4 col-xs-6">
                         <div className="supporter-logo">
-                            <img src="assets/img/sponsors/google.svg" className="img-thumbnail" alt=""/>
+                            <img src="assets/img/supporters/google.svg" className="img-thumbnail" alt=""/>
                         </div>
                     </div>
 
                     <div className="col-lg-3 col-md-4 col-xs-6">
                         <div className="supporter-logo">
-                            <img src="assets/img/sponsors/github.svg" className="img-thumbnail" alt=""/>
+                            <img src="assets/img/supporters/bbc.svg" className="img-thumbnail" alt=""/>
                         </div>
                     </div>
 
                     <div className="col-lg-3 col-md-4 col-xs-6">
                         <div className="supporter-logo">
-                            <img src="assets/img/sponsors/dotsrc.png" className="img-thumbnail" alt=""/>
+                            <img src="assets/img/supporters/plex.svg" className="img-thumbnail" alt=""/>
                         </div>
                     </div>
 
                     <div className="col-lg-3 col-md-4 col-xs-6">
                         <div className="supporter-logo">
-                            <img src="assets/img/sponsors/osl.png" className="img-thumbnail" alt=""/>
+                            <img src="assets/img/supporters/lastfm.svg" className="img-thumbnail" alt=""/>
                         </div>
                     </div>
 
                     <div className="col-lg-3 col-md-4 col-xs-6">
                         <div className="supporter-logo">
-                            <img src="assets/img/sponsors/microsoft.png" className="img-thumbnail" alt=""/>
+                            <img src="assets/img/supporters/microsoft.png" className="img-thumbnail" alt=""/>
                         </div>
                     </div>
 
                     <div className="col-lg-3 col-md-4 col-xs-6">
                         <div className="supporter-logo">
-                            <img src="assets/img/sponsors/pipedrive.png" className="img-thumbnail" alt=""/>
+                            <img src="assets/img/supporters/pandora.png" className="img-thumbnail" alt=""/>
                         </div>
                     </div>
 
                     <div className="col-lg-3 col-md-4 col-xs-6">
                         <div className="supporter-logo">
-                            <img src="assets/img/sponsors/layer42.png" className="img-thumbnail" alt=""/>
+                            <img src="assets/img/supporters/hubbard.png" className="img-thumbnail" alt=""/>
                         </div>
                     </div>
 
                     <div className="col-lg-3 col-md-4 col-xs-6">
                         <div className="supporter-logo">
-                            <img src="assets/img/sponsors/Amazon_logo.svg" className="img-thumbnail" alt=""/>
+                            <img src="assets/img/supporters/Amazon_logo.svg" className="img-thumbnail" alt=""/>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-3 col-md-4 col-xs-6">
+                        <div className="supporter-logo">
+                            <img src="assets/img/supporters/ticketmaster.svg" className="img-thumbnail" alt=""/>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-3 col-md-4 col-xs-6">
+                        <div className="supporter-logo">
+                            <img src="assets/img/supporters/umg.svg" className="img-thumbnail" alt=""/>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-3 col-md-4 col-xs-6">
+                        <div className="supporter-logo">
+                            <img src="assets/img/supporters/siriusxm.jpg" className="img-thumbnail" alt=""/>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-3 col-md-4 col-xs-6">
+                        <div className="supporter-logo">
+                            <img src="assets/img/supporters/mc.svg" className="img-thumbnail" alt=""/>
                         </div>
                     </div>
 
